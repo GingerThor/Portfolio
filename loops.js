799bee9fcdd571f607bbacea80c3f4f05a059376
jdboy83 <jdboy8390@gmail.com>
2024-01-21 21:23:35 -0600

    testing theory

--- loops.js
+++ loops.js
@@ -1,13 +1,15 @@
 let number = 5;
 let i = 1;
 
-while (i <= 5)
-{
-    if (number % 2 == 1)
-    { 
-        document.write("<p>" + number + "/n</p>");
-    }
-    number++;
-    i++;
-}
+var test = number % 2;
+document.write(test)
+//while (i <= 5)
+//{
+//    if (number % 2 == 1)
+//    { 
+//        document.write("<p>" + number + "/n</p>");
+//    }
+//    number++;
+//    i++;
+//}
 document.write("Hello World");
