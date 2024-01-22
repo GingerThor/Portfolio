d093304a192f758c3bab087dfc6c28452f219c02
jdboy83 <jdboy8390@gmail.com>
2024-01-21 21:34:43 -0600

    test 3

--- loops.js
+++ loops.js
@@ -1,15 +1,13 @@
 let number = 5;
 let i = 1;
 
-var test = number % 2;
-document.write(test);
-//while (i <= 5)
-//{
-//    if (number % 2 == 1)
-//    { 
-//        document.write("<p>" + number + "/n</p>");
-//    }
-//    number++;
-//    i++;
-//}
-//document.write("Hello World");
+while (i <= 5)
+{
+    var test = number % 2;
+    if (test == 1)
+    { 
+        document.write("<p>" + number + "/n</p>");
+    }
+    number++;
+    i++;
+}
