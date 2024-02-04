f733f0acb21a9e3d37c1085a908f2a5c3df2683f
jdboy83 <jdboy8390@gmail.com>
2024-02-04 14:42:32 -0600

    Created Paragraph Node

--- dom.js
+++ dom.js
@@ -3,10 +3,14 @@ const divText = div.textContent;
 
 for (let count = 1; count <= 5; count ++)
 {
+    const newParagraph = document.createElement("p");
     let text  = "";
     for (let innerCount = count; innerCount > 0; innerCount --)
     {
         text = text + "*";
     }
+    const paragraphText = document.createTextNode(text);
+    newParagraph.appendChild(paragraphText);
+    div.appendChild(newParagraph);
     document.write(`<p>${text}</p>`);
 }
