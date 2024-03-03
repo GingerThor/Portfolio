1a6154801260208827f0c30f355a892a4f712b14
jdboy83 <jdboy8390@gmail.com>
2024-03-03 15:08:00 -0600

    I think I found it

--- Unit 2/Closures/closures.js
+++ Unit 2/Closures/closures.js
@@ -1,32 +1,35 @@
-function CreateCharacter (Name, HP, ...Moves) {
-    let hp = HP;
-    let name = Name;
-    let moves = Moves;
+document.addEventListener("DOMContentLoaded", () => 
+{
+    function CreateCharacter (Name, HP, ...Moves) {
+        let hp = HP;
+        let name = Name;
+        let moves = Moves;
 
-    return {
-        getHP() {
-            return hp;
-        },
-        getName() {
-            return name;
-        },
-        getMoves() {
-            var attacks = document.querySelector("div");
-            for(var move of moves)
-            {
-                const newParagraph = document.createElement("p");
-                let text = move;
-                const paragraphText = document.createTextNode(text);
-                newParagraph.appendChild(paragraphText);
-                attacks.appendChild(newParagraph);
+        return {
+            getHP() {
+                return hp;
+            },
+            getName() {
+                return name;
+            },
+            getMoves() {
+                var attacks = document.querySelector(".moves");
+                for(var move of moves)
+                {
+                    const newParagraph = document.createElement("p");
+                    let text = move;
+                    const paragraphText = document.createTextNode(text);
+                    newParagraph.appendChild(paragraphText);
+                    attacks.appendChild(newParagraph);
+                }
+                return moves;
             }
-            return moves;
-        }
-    };
-}
+        };
+    }
 
-var NPC = CreateCharacter("Bob", 23, "Bob's Spinning Slash", "Bob's Screaming Death", "Bob's Decay", "Bob's Return");
+    var NPC = CreateCharacter("Bob", 23, "Bob's Spinning Slash", "Bob's Screaming Death", "Bob's Decay", "Bob's Return");
 
-NPC.getMoves();
-console.log(NPC.getHP());
-console.log(NPC.getName());
+    NPC.getMoves();
+    console.log(NPC.getHP());
+    console.log(NPC.getName());
+});
