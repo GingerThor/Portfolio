1f3fb269ea1acf24f69378c91628defbbe710fe9
jdboy83 <jdboy8390@gmail.com>
2024-03-03 14:01:03 -0600

    testing some new code for closures and rest

---
+++ Unit 2/Closures/closures.js
@@ -0,0 +1,26 @@
+function CreateFunction (Name, HP, ...Moves)
+{
+    let hp = HP;
+    let name = Name;
+    let moves = Moves;
+
+    return 
+    {
+        getHP()
+        {
+            return hp;
+        }
+        getName()
+        {
+            return name;
+        }
+        getMoves()
+        {
+            return moves;
+        }
+    };
+}
+
+var NPC = CreateFunction("Bob", 23, "Bob's Spinning Slash", "Bob's Screaming Death");
+
+document.write(`<p>${NPC.getMoves}</p>`);
